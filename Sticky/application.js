b92bc4aed4710a6435f212f9349f0afa8c2907e5
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2020-08-28 14:05:05 +0530

    Add files via upload

---
+++ Sticky/application.js
@@ -0,0 +1,12 @@
+function open_overlay(){
+    document.getElementById("overlay").style.display = 'block';
+}
+function close_overlay(){
+    document.getElementById("overlay").style.display='none';
+}
+function open_update_overlay(){
+    document.getElementById("overlay-update").style.display = 'block';
+}
+function close_update_overlay(){
+    document.getElementById("overlay-update").style.display='none';
+}
